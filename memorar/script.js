4b35e283688db575b1f6a05862db3c90673789fb
nabilah24 <nabilah.zakiyah.2205356@students.um.ac.id>
2024-11-12 22:35:45 +0700

    test

--- memorar/script.js
+++ memorar/script.js
@@ -15,35 +15,40 @@ videoEntities.forEach(videoEntity => {
   };
 });
 
-const raccoonVideo = document.getElementById("raccoonModel");
-const bearVideo = document.getElementById("bearModel");
-const bearAudio = document.getElementById("bearAudio");
+const video1 = document.getElementById("vid1");
+const video2 = document.getElementById("vid2");
+const audio1 = document.getElementById("vid1Audio");
+const audio2 = document.getElementById("vid2Audio");
 
 // Tambahkan event listener pada touch untuk memulai video jika belum
 document.body.addEventListener('click', () => {
-  if (raccoonVideo.paused) raccoonVideo.play();
-  if (bearVideo.paused) bearVideo.play();
-  bearAudio.pause();
+  if (video1.paused) video1.play();
+  if (video2.paused) video2.play();
+  audio1.pause();
+  audio2.pause();
 });
 
 // Event listener MindAR untuk kontrol target
 document.querySelector('[mindar-image-target="targetIndex: 0"]').addEventListener("targetFound", () => {
-  raccoonVideo.play();
+  video1.play();
+  audio1.play();
 });
 
 document.querySelector('[mindar-image-target="targetIndex: 0"]').addEventListener("targetLost", () => {
-  raccoonVideo.pause();
-  raccoonVideo.currentTime = 0;
+  video1.pause();
+  video1.currentTime = 0;
+  audio1.pause();
+  audio1.currentTime = 0;
 });
 
 document.querySelector('[mindar-image-target="targetIndex: 1"]').addEventListener("targetFound", () => {
-  bearVideo.play();
-  bearAudio.play();
+  video2.play();
+  audio2.play();
 });
 
 document.querySelector('[mindar-image-target="targetIndex: 1"]').addEventListener("targetLost", () => {
-  bearVideo.pause();
-  bearVideo.currentTime = 0;
-  bearAudio.pause();
-  bearAudio.currentTime = 0;
+  video2.pause();
+  video2.currentTime = 0;
+  audio2.pause();
+  audio2.currentTime = 0;
 });
